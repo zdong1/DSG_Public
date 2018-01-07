15ad0b35db3ba775040bbc0ac65e88fd768ce977
bigz <zhdong2@gmail.com>
2018-01-06 21:06:35 -0800

    Update map.R

--- DL/Codes/map.R
+++ DL/Codes/map.R
@@ -1,13 +1,17 @@
+# This is initially an interative map/table I wrote for Zillow Challenges
+# For confidentiality reasons, key variables have been removed (V1, DV1, CV1, etc. are all variables)
+# Zhihang Dong
 library("leaflet")
 library("tidyverse")
 library("htmltools")
 library("data.table")
-train_df <- fread("training_CONFIDENTIAL.csv", showProgress=FALSE)
+# Using Leaflet to do interactive mapping
+train_df <- fread("yourdata.csv", showProgress=FALSE)
 train_df[, 
-         list(label=HTML(paste(sep="<br>",paste("Bedrooms:", BedroomCnt),
-                               paste("Bathrooms:", BathroomCnt),
-                               paste("Total area:", FinishedSquareFeet),
-                               paste("Sales Price: $", SaleDollarCnt)))), 
+         list(label=HTML(paste(sep="<br>",paste("v1:", v1),
+                               paste("v2:", v2),
+                               paste("v3:", v3),
+                               paste("v4: $", v4)))), 
        list(Longitude, Latitude)] %>% leaflet() %>% addTiles() %>%addCircleMarkers(
          lat =  ~ Latitude / 1e6,
          lng =  ~ Longitude / 1e6,
@@ -15,8 +19,8 @@ train_df[,
          clusterOptions = markerClusterOptions()
          )
 
-train_df$CountyID<- as.numeric(train_df$ZoneCodeCounty)
+train_df$someID<- as.numeric(train_df$somecode)
 
 # Make an interactive query table for different built years
-train_df[, list(n=.N, Mean.Price=mean(SaleDollarCnt), SD.Price=sd(SaleDollarCnt)), 
-         by=BuiltYear] %>%round(3) %>% datatable()
+train_df[, list(n=.N, Mean.Price=mean(DV1), SD.Price=sd(DV1)), 
+         by=CV1] %>%round(3) %>% datatable()
